8264146f3133c4da551f2117e0fc44a8bf53c5c3
Karim ElSedfy <kariemelsedfy@gmail.com>
2025-07-21 13:31:50 +0300

    shufflign anime list before sending now. Only thing that's left to do is test and upload it

--- routes/getSuggestedAnimes.js
+++ routes/getSuggestedAnimes.js
@@ -8,13 +8,26 @@ const getUserID = require('./builtSuggestedAnimeList/getUserID');
 const getSuggestedAnimeList = require('../database/getSuggestedAnimeList');
 
 
+function shuffleArray(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    // swap arr[i] and arr[j]
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
 router.post('/', async (req, res) => {
     try {
         const token = req.body.MAL_ACCESS_TOKEN;
         const userID = await getUserID(token);
 
         const suggestedAnimeList = await getSuggestedAnimeList(userID);
-        
+        shuffleArray(suggestedAnimeList);
+
         if (suggestedAnimeList.length >= 20) {
             res.send(suggestedAnimeList.slice(0, 20));
         } else {
